aca0e98a73680f775b54f0c35197eed0af9170a2
jorzel1988 <jaroslaw.orzel@emplocity.pl>
2017-12-20 11:02:50 +0100

    30_days_of_code; day12-19

---
+++ 30_days_of_code/day12_inheritance.py
@@ -0,0 +1,85 @@
+﻿"""
+Objective 
+Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video!
+
+Task 
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string, .
+A string, .
+An integer, .
+An integer array (or vector) of test scores, .
+A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
+Grading.png
+
+Input Format
+
+The locked stub code in your editor calls your Student class constructor and passes it the necessary arguments. It also calls the calculate method (which takes no arguments).
+
+You are not responsible for reading the following input from stdin: 
+The first line contains , , and , respectively. The second line contains the number of test scores. The third line of space-separated integers describes .
+
+Constraints
+
+Output Format
+
+This is handled by the locked stub code in your editor. Your output will be correct if your Student class constructor and calculate() method are properly implemented.
+
+Sample Input
+
+Heraldo Memelli 8135627
+2
+100 80
+Sample Output
+
+ Name: Memelli, Heraldo
+ ID: 8135627
+ Grade: O
+Explanation
+
+This student had  scores to average:  and . The student's average grade is . An average grade of  corresponds to the letter grade , so our calculate() method should return the character'O'.
+"""
+
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
+      def printPerson(self):
+        print("Name:", self.lastName + ",", self.firstName)
+        print("ID:", self.idNumber)
+
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    def __init__(self, firstName, lastName, idNumber, scores):
+        super(Student, self).__init__(firstName, lastName, idNumber)
+        self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        average = sum(scores) / (1.0 * len(scores))
+        if(average < 40):
+            return 'T'
+        elif average >= 40 and average < 55:
+            return 'D'
+        elif average >= 55 and average < 70:
+            return 'P'
+        elif average >= 70 and average < 80:
+            return 'A'
+        elif average >= 80 and average < 90:
+            return 'E'
+        else:
+            return 'O'
---
+++ 30_days_of_code/day19_interfaces.cpp
@@ -0,0 +1,67 @@
+﻿"""
+Objective 
+Today, we're learning about Interfaces. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task 
+The AdvancedArithmetic interface and the method declaration for the abstract int divisorSum(int n) method are provided for you in the editor below. Write the Calculator class, which implements the AdvancedArithmetic interface. The implementation for the divisorSum method must be public and take an integer parameter, , and return the sum of all its divisors.
+
+Note: Because we are writing multiple classes in the same file, do not use an access modifier (e.g.: public) in your class declaration (or your code will not compile); however, you must use the public access modifier before your method declaration for it to be accessible by the other classes in the file.
+
+Input Format
+
+A single line containing an integer, .
+
+Constraints
+
+Output Format
+
+You are not responsible for printing anything to stdout. The locked Solution class in the editor below will call your code and print the necessary output.
+
+Sample Input
+
+6
+Sample Output
+
+I implemented: AdvancedArithmetic
+12
+Explanation
+
+The integer  is evenly divisible by , , , and . Our divisorSum method should return the sum of these numbers, which is . The Solution class then prints  on the first line, followed by the sum returned by divisorSum (which is ) on the second line.
+"""
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class AdvancedArithmetic{
+    public:
+        virtual int divisorSum(int n)=0;
+};
+
+//Write your code here
+
+class Calculator: public AdvancedArithmetic {
+    public:
+        int divisorSum(int n) {
+            int total = 0;
+            for(int i = 1; i <= n; i++ ) {
+                if ((n % i) == 0)
+                    total += i;
+            }
+            return total;
+        }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    AdvancedArithmetic *myCalculator = new Calculator(); 
+    int sum = myCalculator->divisorSum(n);
+    cout << "I implemented: AdvancedArithmetic\n" << sum;
+    return 0;
+}
